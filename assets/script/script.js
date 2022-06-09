61cb3b0336422ec6e643aaaaf79156cfa41e675d
Ph-uad <temidayo26j@gmail.com>
2022-06-09 21:26:34 +0100

    kk

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,5 @@
 
 let tl = gsap.timeline();
-
-
-tl.from('.heading-jumbo', {opacity: 0,x: 200, duration:1.3,ease:"power2"});
+tl.from('.heading-jumbo', {opacity: 0, scale: 0.1 , duration:1.7,ease:"power2"});
 tl.from('.header-subtext', {opacity: 0 ,x: -20, duration:1,ease:"power2"});
-tl.from('.button', {opacity: 0 ,y: 100, ease:"power2"});
-
-
-
-
+tl.from('.button', {opacity: 0 ,y: 100, ease:"sine"});
