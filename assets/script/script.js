8eb1b2390395f34075201785fc005497bd6bc3b8
Ph-uad <temidayo26j@gmail.com>
2022-06-09 06:40:41 +0100

    styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,28 +8,28 @@ tl.from('.header-subtext', {opacity: 0 ,x: -20, duration:1,ease:"power2"});
 tl.from('.button', {opacity: 0 ,y: 20, ease:"power2"});
 
 
-ScrollTrigger.create({
-    trigger: ".header-container",
-    start: "top top",
-    pin: true,
-    scrub: 1,
-    endTrigger: '.button',
-    end: "110 top",
-});
+// ScrollTrigger.create({
+//     trigger: ".header-container",
+//     start: "top top",
+//     pin: true,
+//     scrub: 1,
+//     endTrigger: '.button',
+//     end: "110 top",
+// });
 
-//sun motion 
-gsap.to(".header-container", {opacity: 1})
-
-
+// //sun motion 
+// gsap.to(".header-container", {opacity: 1})
 
 
-const cards = document.querySelectorAll('.card')
 
-cards.forEach( card => {
+
+// const cards = document.querySelectorAll('.card')
+
+// cards.forEach( card => {
     
-    card.addEventListener('click', ()=>{
-        card.classList.toggle('flip')
-    })
-});
+//     card.addEventListener('click', ()=>{
+//         card.classList.toggle('flip')
+//     })
+// });
 
 
