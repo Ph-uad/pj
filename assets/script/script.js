c0958dd3b2cebadce792eb2880f5a07ac4685c44
Ph-uad <temidayo26j@gmail.com>
2022-06-09 14:37:52 +0100

    mobile-view

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,11 @@
-gsap.registerPlugin(ScrollTrigger);
 
 let tl = gsap.timeline();
 
 
-tl.from('.heading-jumbo', {opacity: 0,y: -20, duration:1.3,ease:"power2"});
+tl.from('.heading-jumbo', {opacity: 0,x: 200, duration:1.3,ease:"power2"});
 tl.from('.header-subtext', {opacity: 0 ,x: -20, duration:1,ease:"power2"});
-tl.from('.button', {opacity: 0 ,y: 20, ease:"power2"});
+tl.from('.button', {opacity: 0 ,y: 100, ease:"power2"});
 
 
-// ScrollTrigger.create({
-//     trigger: ".header-container",
-//     start: "top top",
-//     pin: true,
-//     scrub: 1,
-//     endTrigger: '.button',
-//     end: "110 top",
-// });
-
-// //sun motion 
-// gsap.to(".header-container", {opacity: 1})
-
-
-
-
-// const cards = document.querySelectorAll('.card')
-
-// cards.forEach( card => {
-    
-//     card.addEventListener('click', ()=>{
-//         card.classList.toggle('flip')
-//     })
-// });
 
 
