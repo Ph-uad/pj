6b2850e7e9035acd6bc2e2fb23130628b5fd7b6b
Ph-uad <temidayo26j@gmail.com>
2022-06-13 09:33:37 +0100

    aesthetic shift

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,79 @@
 
 let tl = gsap.timeline();
-tl.from('.heading-jumbo', {opacity: 0, scale: 0.1 , duration:1.7,ease:"power2"});
-tl.from('.header-subtext', {opacity: 0 ,x: -20, duration:1,ease:"power2"});
-tl.from('.button', {opacity: 0 ,y: 100, ease:"sine"});
+tl.from('.button', {delay:2, y: 1000, ease:"power2"});
 
 
 const videos =  document.querySelectorAll('.media-container__video');
 const buttonPrevious = document.querySelector('.media-icon__left');
 const buttonNext = document.querySelector('.media-icon__right');
 let currentSlide  = 0;
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.to(".header-container" , {
+    scrollTrigger : {
+        trigger: ".header-container",
+        start : "top top",
+        pin: true,
+        scrub: 4,
+        endTrigger : ".header",
+        end: "bottom center"
+     },
+})
+
+gsap.to(".header-gallery" , {
+    scrollTrigger : {
+        trigger: ".header-gallery",
+        start : "top center",
+        scrub : 3,
+        end: "bottom +=400"
+     },
+     y: 70
+})
+
+gsap.to(".nav" , {
+    scrollTrigger : {
+        trigger: ".fluid-container",
+        start : "top top",
+        scrub : 2,
+        end: "-=200"
+     },
+     backgroundColor : "#1d1d1d40",
+     color: "#fff",
+     opacity: 1,
+})
+
+// const sections = document.querySelectorAll('.section')
+// console.log(sections)
+
+// sections.forEach(e => {
+//     gsap.from(e , {
+//         scrollTrigger : {
+//             trigger: e,
+//             start : "top center",
+//             markers: true,
+//             end: "-=200"
+//          },
+//         x:-800,
+//         opacity: 0
+//     })
+// });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.ml3');
+
+textWrapper.forEach(text => {
+    text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+});
+
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 20,
+    delay: (el, i) => 50 * (i+1)
+  })
 
 
 const slide = ( ) =>{
