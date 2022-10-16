cdca912bc3169e0ea17515a0f637bd26cd851d5f
Ph-uad <temidayo26j@gmail.com>
2022-10-16 10:26:17 +0100

    first commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,62 +1,79 @@
-
-let tl = gsap.timeline();
-tl.from('.button', {delay:2, y: 1000, ease:"power2"});
-
-
-const videos =  document.querySelectorAll('.media-container__video');
-const buttonPrevious = document.querySelector('.media-icon__left');
-const buttonNext = document.querySelector('.media-icon__right');
-let currentSlide  = 0;
+const media = document.querySelectorAll('.media');
+const video = document.querySelectorAll('.media__video');
 
 gsap.registerPlugin(ScrollTrigger)
-gsap.to(".header-container" , {
-    scrollTrigger : {
-        trigger: ".header-container",
-        start : "top top",
-        pin: true,
+
+gsap.to(".nav", {
+    scrollTrigger: {
+        trigger: ".header",
+        start: "top top",
+        scrub: 2,
+        end: "+=1000"
+    },
+    backgroundColor: "#1d1d1d40",
+    color: "#fff",
+    opacity: 1,
+})
+
+gsap.to(".paragraph", {
+    scrollTrigger: {
+        trigger: ".ad__block",
+        start: "top center",
         scrub: 4,
-        endTrigger : ".header",
-        end: "bottom center"
-     },
+        end: "bottom -=200",
+    },
+    y: 70,
 })
 
-gsap.to(".header-gallery" , {
-    scrollTrigger : {
-        trigger: ".header-gallery",
-        start : "top center",
-        scrub : 3,
-        end: "bottom +=400"
-     },
-     y: 70
+gsap.to(".media__video", {
+    scrollTrigger: {
+        trigger: ".media",
+        start: "top center",
+        scrub: 4,
+        end: "bottom -=200",
+        onEnter: () => {video[0].play() },
+        onLeave: () => { video[0].pause() },
+
+    },
+    width: "100vw",
 })
 
-gsap.to(".nav" , {
-    scrollTrigger : {
-        trigger: ".fluid-container",
-        start : "top top",
-        scrub : 2,
-        end: "-=200"
-     },
-     backgroundColor : "#1d1d1d40",
-     color: "#fff",
-     opacity: 1,
-})
+const videos = document.querySelectorAll('.media__video')
+const intros = document.querySelectorAll('.heading--section')
 
-// const sections = document.querySelectorAll('.section')
-// console.log(sections)
+videos.forEach(e => {
+    gsap.to(e, {
+        scrollTrigger: {
+            trigger: e,
+            start: "center +=700",
+            toggleActions: "play pause resume reset",
+            scrub: 4,
+            end: "bottom +=400",
+            onEnter: () => { e.play(); },
+            onLeave: () => { e.pause(); },
+        },
+        transform: "scale(1)"
+    })
+});
 
-// sections.forEach(e => {
-//     gsap.from(e , {
-//         scrollTrigger : {
-//             trigger: e,
-//             start : "top center",
-//             markers: true,
-//             end: "-=200"
-//          },
-//         x:-800,
-//         opacity: 0
-//     })
-// });
+
+
+intros.forEach(e => {
+    gsap.from(e, {
+        scrollTrigger: {
+            trigger: e,
+            start: "top center",
+            scrub: 3,
+            toggleActions: "play pause resume reset",
+            end: "bottom +=400",
+        },
+        opacity: 0,
+        y: 10,
+        transform: "scale(.7)"
+    })
+});
+
+
 
 // Wrap every letter in a span
 var textWrapper = document.querySelectorAll('.ml3');
@@ -71,26 +88,54 @@ anime.timeline({loop: false})
     targets: '.ml3 .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
-    duration: 20,
+    duration: 200,
     delay: (el, i) => 50 * (i+1)
   })
 
 
-const slide = ( ) =>{
-    videos.forEach((block, index) => {
-        block.style.transform = `translateX(${100 * (index - currentSlide)}%)`; 
-    });
-}
+// const slide = ( ) =>{
+//     videos.forEach((block, index) => {
+//         block.style.transform = `translateX(${100 * (index - currentSlide)}%)`; 
+//     });
+// }
 
-slide()
+// slide()
 
 
-buttonPrevious.addEventListener('click',()=>{
-   currentSlide === 0 ? currentSlide  = videos.length -1 : currentSlide--;
-   slide();
-})
+// buttonPrevious.addEventListener('click',()=>{
+//    currentSlide === 0 ? currentSlide  = videos.length -1 : currentSlide--;
+//    slide();
+// })
 
-buttonNext.addEventListener('click',()=>{
-    currentSlide === videos.length -1 ? currentSlide = 0 : currentSlide++;
-    slide();
-})
+// buttonNext.addEventListener('click',()=>{
+//     currentSlide === videos.length -1 ? currentSlide = 0 : currentSlide++;
+//     slide();
+// })
+// let container = gsap.utils.toArray(".container"),scrollTween;
+
+// function goToSection(i) {
+//     scrollTween = gsap.to(window, {
+//         scrollTo: { y: i * innerHeight, autoKill: false },
+//         duration: 1,
+//         onComplete: () => scrollTween = null,
+//         overwrite: true
+//     });
+// }
+
+// container.forEach((container, i) => {
+//     ScrollTrigger.create({
+//         trigger: container,
+//         start: "top bottom",
+//         markers: true,
+//         end: "bottom bottom",
+//         onToggle: self => self.isActive && !scrollTween && goToSection(i)
+//     });
+// });
+
+// 
+// just in case the user forces the scroll to an inbetween spot (like a momentum scroll on a Mac that ends AFTER the scrollTo tween finishes):
+// ScrollTrigger.create({
+//     start: 0,
+//     end: "max",
+//     snap: .8 / (container.length - 1)
+// })
